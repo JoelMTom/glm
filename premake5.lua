cffb05dbcf5ec8db402c7999d2bfbc124202be99
JoelMTom <joelqwerty007@gmail.com>
2024-05-12 21:28:01 +0530

    add premake5

---
+++ premake5.lua
@@ -0,0 +1,17 @@
+---@diagnostic disable: undefined-global, undefined-field
+
+local projectName = path.getbasename(os.getcwd())
+
+project (projectName)
+  kind "StaticLib"
+  language "C"
+
+	targetdir ("%{wks.location}/bin/" .. Outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. Outputdir .. "/%{prj.name}")
+
+  files {
+    "glm/**.h"
+  }
+
+  IncludedirsDep[projectName] = os.getcwd() .. "/glm/"
+  -- Linklib[projectName] = projectName
