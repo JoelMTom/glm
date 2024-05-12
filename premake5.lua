de467a8f8d434fa0ed3ad671dcfdd5802352618f
JoelMTom <joelqwerty007@gmail.com>
2024-05-12 21:30:48 +0530

    update premake5

--- premake5.lua
+++ premake5.lua
@@ -4,7 +4,7 @@ local projectName = path.getbasename(os.getcwd())
 
 project (projectName)
   kind "StaticLib"
-  language "C"
+  language "C++"
 
 	targetdir ("%{wks.location}/bin/" .. Outputdir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin-int/" .. Outputdir .. "/%{prj.name}")
